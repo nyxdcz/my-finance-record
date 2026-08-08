083b753cc3b6a9da44c67dbeab901959d1c07fa0
nyco <nyxdcz@gmail.com>
2026-08-08 23:16:21 +0800

    Clarify revision deadline workflow

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 "use strict";
 const APP_VERSION = "13.0.18";
-const CACHE_VERSION = "finance-v13-20260808-v13018-payment-safeguard-r2";
+const CACHE_VERSION = "finance-v13-20260808-v13018-revision-deadline-r3";
 const SHELL_CACHE = `${CACHE_VERSION}-shell`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 const DB_NAME = "simple-finance-project-records-v12-db";
